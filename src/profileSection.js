acdfec783efb7560d48c725800d2cc81c62fa898
OLOWOOKEREISAAC <isaacolowookere1@gmail.com>
2022-10-30 09:31:09 +0100

    added the share and more btn

--- src/profileSection.js
+++ src/profileSection.js
@@ -1,12 +1,20 @@
 import './profileSection.css';
 import profilePic from "./assets/pic.png";
+import sharebtn from "./assets/share.png";
+import morebtn from "./assets/more.png";
 
 
 
 const ProfileSection = () => {
     return(
         <>
-            <img src={profilePic} alt="profile pic" id="profile__img"></img>
+            <div className='profileContainer'>
+                <img src={profilePic} alt="profile pic" id="profile__img"></img>
+                <img src={sharebtn} alt="" id="sharebtn"/>
+                <img src={morebtn} alt="" id="morebtn"></img>
+            </div>
+           
+
            
             <p id="twitter">OLOWOOKEREISAAC</p>
             <p id="slack">_isaac_ </p>
